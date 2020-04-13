ba834888ffcb4f8feaf68bac94c564e15a530e72
AlbertJG <31087005+AlbertJG@users.noreply.github.com>
2020-04-12 23:55:23 -0500

    Added email function
    - Email function works when first creating an event. However, it is currently not set up for when you update an event.
    - Need to look into delaying the email as well, using CRON jobs.
    - Need to still also check tags to send emails to those that want them.

--- pardpush_proj/notification/forms.py
+++ pardpush_proj/notification/forms.py
@@ -5,6 +5,8 @@ from django.forms.utils import ValidationError
 
 from notification.models import (Student, Tag, User, Event)
 
+from django.core import mail
+from django.core.mail import send_mail, EmailMessage
 
 class OrganizerSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -19,6 +21,9 @@ class OrganizerSignUpForm(UserCreationForm):
 
 
 class StudentSignUpForm(UserCreationForm):
+    # Email field
+    email = forms.EmailField(required=True, help_text='Must be a valid lafayette.edu or gmail account.')
+    
     interests = forms.ModelMultipleChoiceField(
         queryset=Tag.objects.all(),
         widget=forms.CheckboxSelectMultiple,
@@ -63,3 +68,30 @@ class TagSelectForm(forms.ModelForm):
             'tag': forms.CheckboxSelectMultiple,
             'data': DateForm,
         }
+        
+    # Method for sending email notifications to users
+    def send_notification(self, request, queryset):
+        connection = mail.get_connection()
+
+        for obj in User.objects.all():
+            field_name = 'is_student'
+            field_object = User._meta.get_field(field_name)
+            field_value = getattr(obj, field_object.attname)
+            
+            if(str(field_value) == "True"):
+                field_name = 'email'
+                field_object = User._meta.get_field(field_name)
+                field_value = getattr(obj, field_object.attname)
+                
+                # Construct the body of the email
+                email = EmailMessage(
+                            queryset.cleaned_data['name'],
+                            'Event Date: ' + str(queryset.cleaned_data['date']) + '\n' 
+                            + 'Event Location: ' + queryset.cleaned_data['location'] + '\n'
+                            + '\n' + queryset.cleaned_data['message'], 
+                            'pardpushhost@gmail.com', # Host gmail
+                            [str(field_value)], # Recepient
+                    )
+                email.send()
+                
+        connection.close()
