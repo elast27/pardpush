46c136535e2573eb42d9c214d23e2ce2b01ebc65
wangme <wangme@lafayette.edu>
2020-04-29 03:28:56 -0400

    local settings

--- pardpush_proj/pardpush/settings.py
+++ pardpush_proj/pardpush/settings.py
@@ -161,3 +161,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'pardpushhost@gmail.com'
 EMAIL_HOST_PASSWORD = 'holozuxbsprorhvy'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
