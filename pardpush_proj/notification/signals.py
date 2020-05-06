91a9a365f120e725b2829f5148ec61013c20cd03
Matthew Stern <matthewstern@Matthews-MBP.home>
2020-05-06 19:08:46 -0400

    signal handling for role assignment

---
+++ pardpush_proj/notification/signals.py
@@ -0,0 +1,12 @@
+from django_cas_ng.signals import cas_user_authenticated
+from django.dispatch import receiver
+
+@receiver(cas_user_authenticated)
+def update_role(sender, user=None, attributes=None, **kwargs):
+    if created:
+        role = attributes['eduPersonEntitlement'].split('/')[3]
+        if role == 'faculty':
+            user.is_organizer = True
+        else:
+            user.is_student = True
+        user.save()
