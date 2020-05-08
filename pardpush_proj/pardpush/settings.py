13296dffa8f9b87f1f9de8475b4cf6d2855b9fe4
Eric Last <laste@lafayette.edu>
2020-05-08 01:43:18 -0400

    redis

--- pardpush_proj/pardpush/settings.py
+++ pardpush_proj/pardpush/settings.py
@@ -142,6 +142,18 @@ RQ_QUEUES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
