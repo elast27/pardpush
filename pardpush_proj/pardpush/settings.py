6651e3c2847b835e613e615d24f21862da357a8c
Eric Last <laste@lafayette.edu>
2020-05-08 00:46:23 -0400

    redis queue

--- pardpush_proj/pardpush/settings.py
+++ pardpush_proj/pardpush/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django_cas_ng',
     'tempus_dominus',
+    'django_rq',
+    'scheduler',
 ]
 
 MIDDLEWARE = [
@@ -110,6 +112,35 @@ WSGI_APPLICATION = 'pardpush.wsgi.application'
 #         'PORT': ''
 #     }
 # }
+
+RQ_QUEUES = {
+    'default': {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'DB': 0,
+        'PASSWORD': 'some-password',
+        'DEFAULT_TIMEOUT': 360,
+    },
+    'with-sentinel': {
+        'SENTINELS': [('localhost', 26736), ('localhost', 26737)],
+        'MASTER_NAME': 'redismaster',
+        'DB': 0,
+        'PASSWORD': 'secret',
+        'SOCKET_TIMEOUT': None,
+        'CONNECTION_KWARGS': {
+            'socket_connect_timeout': 0.3
+        },
+    },
+    'high': {
+        'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
+        'DEFAULT_TIMEOUT': 500,
+    },
+    'low': {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'DB': 0,
+    }
+}
 
 
 # Internationalization
